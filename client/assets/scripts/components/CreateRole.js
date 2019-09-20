6d464f6abbd02d6eb1ad4da02d87c79cbca6532b
iucgnaw <iucgnaw@msn.com>
2019-09-20 12:25:15 +0800

    Changed random names to real player's names Changed title to ChangHuaMahjong

--- client/assets/scripts/components/CreateRole.js
+++ client/assets/scripts/components/CreateRole.js
@@ -16,39 +16,21 @@ cc.Class({
     
     onRandomBtnClicked:function(){
         var names = [
-            "上官",
-            "欧阳",
-            "东方",
-            "端木",
-            "独孤",
-            "司马",
-            "南宫",
-            "夏侯",
-            "诸葛",
-            "皇甫",
-            "长孙",
-            "宇文",
-            "轩辕",
-            "东郭",
-            "子车",
-            "东阳",
-            "子言",
+            "王璀",
+            "李神",
+            "涂勇",
+            "祝悦琼",
+            "叶振凯",
+            "黎杞昌",
+            "李沁晖",
+            "周波",
+            "叶骅",
+            "吴皓",
+            "陈擘",
         ];
         
-        var names2 = [
-            "雀圣",
-            "赌侠",
-            "赌圣",
-            "稳赢",
-            "不输",
-            "好运",
-            "自摸",
-            "有钱",
-            "土豪",
-        ];
         var idx = Math.floor(Math.random() * (names.length - 1));
-        var idx2 = Math.floor(Math.random() * (names2.length - 1));
-        this.inputName.string = names[idx] + names2[idx2];
+        this.inputName.string = names[idx];
     },
 
     // use this for initialization
