8725e235ea577d78b8f3da48e4cbae1804516d71
iucgnaw <iucgnaw@msn.com>
2019-09-20 18:29:28 +0800

    Added firewall rule for IIS deployment

--- server/0.add_firewall_inbound_rules.ps1
+++ server/0.add_firewall_inbound_rules.ps1
@@ -1,5 +1,6 @@
 New-NetFirewallRule -DisplayName "ChangHuaMahjong Account Server (TCP-In 9000)" -Direction Inbound -LocalPort 9000 -RemotePort Any -Protocol TCP -Enabled True
-New-NetFirewallRule -DisplayName "ChangHuaMahjong Client (TCP-In 7456)" -Direction Inbound -LocalPort 7456 -RemotePort Any -Protocol TCP -Enabled True
+New-NetFirewallRule -DisplayName "ChangHuaMahjong Client on Cocos Creator (TCP-In 7456)" -Direction Inbound -LocalPort 7456 -RemotePort Any -Protocol TCP -Enabled True
+New-NetFirewallRule -DisplayName "ChangHuaMahjong Client on IIS (TCP-In 1111)" -Direction Inbound -LocalPort 1111 -RemotePort Any -Protocol TCP -Enabled True
 New-NetFirewallRule -DisplayName "ChangHuaMahjong Client Service (TCP-In 9001)" -Direction Inbound -LocalPort 9001 -RemotePort Any -Protocol TCP -Enabled True
 New-NetFirewallRule -DisplayName "ChangHuaMahjong Dealer API (TCP-In 12581)" -Direction Inbound -LocalPort 12851 -RemotePort Any -Protocol TCP -Enabled True
 New-NetFirewallRule -DisplayName "ChangHuaMahjong Game Server (TCP-In 10000)" -Direction Inbound -LocalPort 10000 -RemotePort Any -Protocol TCP -Enabled True
