728a66c3d8d7e0b18701cd0c711bd01c5042b735
iucgnaw <iucgnaw@msn.com>
2019-09-24 10:52:43 +0800

    Formatted

--- server/game_server/mjutils.js
+++ server/game_server/mjutils.js
@@ -1,179 +1,176 @@
-function checkTingPai(seatData,begin,end){
-	for(var i = begin; i < end; ++i){
+function checkTingPai(seatData, begin, end) {
+	for (var i = begin; i < end; ++i) {
 		//如果这牌已经在和了，就不用检查了
-		if(seatData.tingMap[i] != null){
+		if (seatData.tingMap[i] != null) {
 			continue;
 		}
 		//将牌加入到计数中
 		var old = seatData.countMap[i];
-		if(old == null){
+		if (old == null) {
 			old = 0;
 			seatData.countMap[i] = 1;
-		}
-		else{
-			seatData.countMap[i] ++;		
+		} else {
+			seatData.countMap[i]++;
 		}
 
 		seatData.holds.push(i);
 		//逐个判定手上的牌
 		var ret = checkCanHu(seatData);
-		if(ret){
+		if (ret) {
 			//平胡 0番
 			seatData.tingMap[i] = {
-				pattern:"normal",
-                fan:0
+				pattern: "normal",
+				fan: 0
 			};
 		}
-		
+
 		//搞完以后，撤消刚刚加的牌
 		seatData.countMap[i] = old;
 		seatData.holds.pop();
-	}	
+	}
 }
 
 var kanzi = [];
 var record = false;
-function debugRecord(pai){
-	if(record){
+
+function debugRecord(pai) {
+	if (record) {
 		kanzi.push(pai);
 	}
 }
 
-function matchSingle(seatData,selected){
+function matchSingle(seatData, selected) {
 	//分开匹配 A-2,A-1,A
 	var matched = true;
 	var v = selected % 9;
-	if(v < 2){
+	if (v < 2) {
 		matched = false;
-	}
-	else{
-		for(var i = 0; i < 3; ++i){
+	} else {
+		for (var i = 0; i < 3; ++i) {
 			var t = selected - 2 + i;
 			var cc = seatData.countMap[t];
-			if(cc == null){
+			if (cc == null) {
 				matched = false;
 				break;
 			}
-			if(cc == 0){
+			if (cc == 0) {
 				matched = false;
 				break;
 			}
-		}		
+		}
 	}
 
 
 	//匹配成功，扣除相应数值
-	if(matched){
-		seatData.countMap[selected - 2] --;
-		seatData.countMap[selected - 1] --;
-		seatData.countMap[selected] --;
+	if (matched) {
+		seatData.countMap[selected - 2]--;
+		seatData.countMap[selected - 1]--;
+		seatData.countMap[selected]--;
 		var ret = checkSingle(seatData);
-		seatData.countMap[selected - 2] ++;
-		seatData.countMap[selected - 1] ++;
-		seatData.countMap[selected] ++;
-		if(ret == true){
+		seatData.countMap[selected - 2]++;
+		seatData.countMap[selected - 1]++;
+		seatData.countMap[selected]++;
+		if (ret == true) {
 			debugRecord(selected - 2);
 			debugRecord(selected - 1);
 			debugRecord(selected);
 			return true;
-		}		
+		}
 	}
 
 	//分开匹配 A-1,A,A + 1
 	matched = true;
-	if(v < 1 || v > 7){
+	if (v < 1 || v > 7) {
 		matched = false;
-	}
-	else{
-		for(var i = 0; i < 3; ++i){
+	} else {
+		for (var i = 0; i < 3; ++i) {
 			var t = selected - 1 + i;
 			var cc = seatData.countMap[t];
-			if(cc == null){
+			if (cc == null) {
 				matched = false;
 				break;
 			}
-			if(cc == 0){
+			if (cc == 0) {
 				matched = false;
 				break;
 			}
-		}		
+		}
 	}
 
 	//匹配成功，扣除相应数值
-	if(matched){
-		seatData.countMap[selected - 1] --;
-		seatData.countMap[selected] --;
-		seatData.countMap[selected + 1] --;
+	if (matched) {
+		seatData.countMap[selected - 1]--;
+		seatData.countMap[selected]--;
+		seatData.countMap[selected + 1]--;
 		var ret = checkSingle(seatData);
-		seatData.countMap[selected - 1] ++;
-		seatData.countMap[selected] ++;
-		seatData.countMap[selected + 1] ++;
-		if(ret == true){
+		seatData.countMap[selected - 1]++;
+		seatData.countMap[selected]++;
+		seatData.countMap[selected + 1]++;
+		if (ret == true) {
 			debugRecord(selected - 1);
 			debugRecord(selected);
 			debugRecord(selected + 1);
 			return true;
-		}		
+		}
 	}
-	
-	
+
+
 	//分开匹配 A,A+1,A + 2
 	matched = true;
-	if(v > 6){
+	if (v > 6) {
 		matched = false;
-	}
-	else{
-		for(var i = 0; i < 3; ++i){
+	} else {
+		for (var i = 0; i < 3; ++i) {
 			var t = selected + i;
 			var cc = seatData.countMap[t];
-			if(cc == null){
+			if (cc == null) {
 				matched = false;
 				break;
 			}
-			if(cc == 0){
+			if (cc == 0) {
 				matched = false;
 				break;
 			}
-		}		
+		}
 	}
 
 	//匹配成功，扣除相应数值
-	if(matched){
-		seatData.countMap[selected] --;
-		seatData.countMap[selected + 1] --;
-		seatData.countMap[selected + 2] --;
+	if (matched) {
+		seatData.countMap[selected]--;
+		seatData.countMap[selected + 1]--;
+		seatData.countMap[selected + 2]--;
 		var ret = checkSingle(seatData);
-		seatData.countMap[selected] ++;
-		seatData.countMap[selected + 1] ++;
-		seatData.countMap[selected + 2] ++;
-		if(ret == true){
+		seatData.countMap[selected]++;
+		seatData.countMap[selected + 1]++;
+		seatData.countMap[selected + 2]++;
+		if (ret == true) {
 			debugRecord(selected);
 			debugRecord(selected + 1);
 			debugRecord(selected + 2);
 			return true;
-		}		
+		}
 	}
 	return false;
 }
 
-function checkSingle(seatData){
+function checkSingle(seatData) {
 	var holds = seatData.holds;
 	var selected = -1;
 	var c = 0;
-	for(var i = 0; i < holds.length; ++i){
+	for (var i = 0; i < holds.length; ++i) {
 		var pai = holds[i];
 		c = seatData.countMap[pai];
-		if(c != 0){
+		if (c != 0) {
 			selected = pai;
 			break;
 		}
 	}
 	//如果没有找到剩余牌，则表示匹配成功了
-	if(selected == -1){
+	if (selected == -1) {
 		return true;
 	}
 	//否则，进行匹配
-	if(c == 3){
+	if (c == 3) {
 		//直接作为一坎
 		seatData.countMap[selected] = 0;
 		debugRecord(selected);
@@ -182,11 +179,10 @@ function checkSingle(seatData){
 		var ret = checkSingle(seatData);
 		//立即恢复对数据的修改
 		seatData.countMap[selected] = c;
-		if(ret == true){
+		if (ret == true) {
 			return true;
 		}
-	}
-	else if(c == 4){
+	} else if (c == 4) {
 		//直接作为一坎
 		seatData.countMap[selected] = 1;
 		debugRecord(selected);
@@ -196,20 +192,20 @@ function checkSingle(seatData){
 		//立即恢复对数据的修改
 		seatData.countMap[selected] = c;
 		//如果作为一坎能够把牌匹配完，直接返回TRUE。
-		if(ret == true){
+		if (ret == true) {
 			return true;
 		}
 	}
-	
+
 	//按单牌处理
-	return matchSingle(seatData,selected);
+	return matchSingle(seatData, selected);
 }
 
-function checkCanHu(seatData){
-	for(var k in seatData.countMap){
+function checkCanHu(seatData) {
+	for (var k in seatData.countMap) {
 		k = parseInt(k);
 		var c = seatData.countMap[k];
-		if(c < 2){
+		if (c < 2) {
 			continue;
 		}
 		//如果当前牌大于等于２，则将它选为将牌
@@ -222,7 +218,7 @@ function checkCanHu(seatData){
 		kanzi = [];
 		var ret = checkSingle(seatData);
 		seatData.countMap[k] += 2;
-		if(ret){
+		if (ret) {
 			//kanzi.push(k);
 			//kanzi.push(k);
 			//console.log(kanzi);
@@ -248,14 +244,12 @@ for(k in seatData.tingMap){
 
 exports.checkTingPai = checkTingPai;
 
-exports.getMJType = function(pai){
-      if(id >= 0 && id < 9){
-          return 0;
-      }
-      else if(id >= 9 && id < 18){
-          return 1;
-      }
-      else if(id >= 18 && id < 27){
-          return 2;
-      }
+exports.getMJType = function (pai) {
+	if (id >= 0 && id < 9) {
+		return 0;
+	} else if (id >= 9 && id < 18) {
+		return 1;
+	} else if (id >= 18 && id < 27) {
+		return 2;
+	}
 }
