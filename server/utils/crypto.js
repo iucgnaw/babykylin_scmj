320e4fce4be600b0a4097cf5f1aff2b280d785fd
iucgnaw <iucgnaw@msn.com>
2019-09-29 16:33:10 +0800

    Formatted

--- server/utils/crypto.js
+++ server/utils/crypto.js
@@ -3,13 +3,13 @@ var crypto = require('crypto');
 exports.md5 = function (content) {
 	var md5 = crypto.createHash('md5');
 	md5.update(content);
-	return md5.digest('hex');	
+	return md5.digest('hex');
 }
 
-exports.toBase64 = function(content){
+exports.toBase64 = function (content) {
 	return new Buffer(content).toString('base64');
 }
 
-exports.fromBase64 = function(content){
+exports.fromBase64 = function (content) {
 	return new Buffer(content, 'base64').toString();
 }
