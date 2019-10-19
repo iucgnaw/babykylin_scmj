7cb9cfe87c9f015db0271c372f10f5016a82bf7c
iucgnaw <iucgnaw@msn.com>
2019-10-19 17:14:00 +0800

    Archived

--- server/configs_mac.js
+++ server/configs_mac.js
@@ -1,6 +1,6 @@
 ﻿var HALL_IP = "game.iucgnaw.com"; //如果非本机访问，这里要变
-var HALL_CLIENT_PORT = 9001;
-var HALL_ROOM_PORT = 9002;
+var HALL_CLIENT_PORT = 11110;
+var HALL_ROOM_PORT = 11111;
 
 var ACCOUNT_PRI_KEY = "^&*#$%()@";
 var ROOM_PRI_KEY = "~!@#$(*&^%$&";
@@ -12,7 +12,7 @@ exports.mysql = function () {
 		HOST: "127.0.0.1",
 		USER: "root",
 		PSWD: "Qwer!234", //如果连接失败，请检查这里
-		DB: "db_babykylin", //如果连接失败，请检查这里
+		DB: "Mahjong", //如果连接失败，请检查这里
 		PORT: 3306,
 	}
 }
@@ -20,14 +20,14 @@ exports.mysql = function () {
 //账号服配置
 exports.account_server = function () {
 	return {
-		CLIENT_PORT: 9000,
+		CLIENT_PORT: 11114,
 		HALL_IP: HALL_IP,
 		HALL_CLIENT_PORT: HALL_CLIENT_PORT,
 		ACCOUNT_PRI_KEY: ACCOUNT_PRI_KEY,
 
 		//
 		DEALDER_API_IP: LOCAL_IP,
-		DEALDER_API_PORT: 12581,
+		DEALDER_API_PORT: 11115,
 		VERSION: "20161227",
 		APP_WEB: "http://fir.im/2f17",
 	};
@@ -51,7 +51,7 @@ exports.game_server = function () {
 		SERVER_ID: "001",
 
 		//暴露给大厅服的HTTP端口号
-		HTTP_PORT: 9003,
+		HTTP_PORT: 11113,
 		//HTTP TICK的间隔时间，用于向大厅服汇报情况
 		HTTP_TICK_TIME: 5000,
 		//大厅服IP
@@ -64,6 +64,6 @@ exports.game_server = function () {
 
 		//暴露给客户端的接口
 		CLIENT_IP: HALL_IP,
-		CLIENT_PORT: 10000,
+		CLIENT_PORT: 11112,
 	};
 };
